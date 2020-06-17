4ca27e8f01b07674a569ed14e36ed2b724d7f863
sarpadi <sergiu.arpadi@analog.com>
2020-06-17 20:21:02 +0300

    spi_engine: Added spi_engine/scripts
    Updated ad40xx/zed project

--- projects/ad40xx_fmc/zed/system_bd.tcl
+++ projects/ad40xx_fmc/zed/system_bd.tcl
@@ -1,4 +1,5 @@
 
+source $ad_hdl_dir/library/spi_engine/scripts/spi_engine.tcl
 source $ad_hdl_dir/projects/common/zed/zed_system_bd.tcl
 
 # specify the spi reference clock frequency in MHz
@@ -13,6 +14,15 @@ set adc_resolution 20
 # the max rate should be 1.6 MSPS
 set adc_sampling_rate 1800000
 
+set data_width 32
+set async_spi_clk 1
+set num_cs 1
+set num_sdi 1
+set sdi_delay 2
+set hier_spi_engine spi_ad40xx
+
+spi_engine_bd $hier_spi_engine $data_width $async_spi_clk $num_cs $num_sdi $sdi_delay
+
 source ../common/ad40xx_bd.tcl
 
 #system ID
@@ -21,4 +31,3 @@ ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
 ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
 set sys_cstring "sys rom custom string placeholder"
 sysid_gen_sys_init_file $sys_cstring
-
