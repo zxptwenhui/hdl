aca391cb71e75eefd7ed46a0abaf63245c52e409
AndreiGrozav <andrei.grozav@analog.com>
2020-07-16 16:33:06 +0300

    cn0540_de10nano: Fix I2C

--- projects/cn0540/de10nano/system_top.v
+++ projects/cn0540/de10nano/system_top.v
@@ -161,18 +161,27 @@ module system_top (
 
   // IO Buffers for I2C
 
-  wire i2c_scl_out;
-  wire i2c_scl_in;
-  wire i2c_sda_in;
-  wire i2c_sda_oe;
+  wire i2c1_scl;
+  wire i2c1_scl_oe;
+  wire i2c1_sda;
+  wire i2c1_sda_oe;
 
   wire i2c0_out_data;
   wire i2c0_sda;
   wire i2c0_out_clk;
   wire i2c0_scl_in_clk;
 
-  ALT_IOBUF scl_iobuf (.i(1'b0), .oe(i2c_scl_out), .o(i2c_scl_in), .io(i2c_scl));
-  ALT_IOBUF sda_iobuf (.i(1'b0), .oe(i2c_sda_oe), .o(i2c_sda_in), .io(i2c_sda));
+  ALT_IOBUF scl_iobuf (
+    .i(1'b0),
+    .oe(i2c1_scl_oe),
+    .o(i2c1_scl),
+    .io(i2c_scl));
+
+  ALT_IOBUF sda_iobuf (
+    .i(1'b0),
+    .oe(i2c1_sda_oe),
+    .o(i2c1_sda),
+    .io(i2c_sda));
 
   ALT_IOBUF scl_video_iobuf (
     .i(1'b0),
@@ -249,10 +258,10 @@ module system_top (
     .sys_hps_hps_io_hps_io_spim1_inst_MOSI (spim1_mosi),
     .sys_hps_hps_io_hps_io_spim1_inst_MISO (spim1_miso),
     .sys_hps_hps_io_hps_io_spim1_inst_SS0 (spim1_ss0),
-    .sys_hps_i2c1_sda (i2c_sda),
-    .sys_hps_i2c1_out_data (i2c_out_data),
-    .sys_hps_i2c1_clk_clk (i2c_out_clk),
-    .sys_hps_i2c1_scl_in_clk (i2c_scl),
+    .sys_hps_i2c1_sda (i2c1_sda),
+    .sys_hps_i2c1_out_data (i2c1_sda_oe),
+    .sys_hps_i2c1_clk_clk (i2c1_scl_oe),
+    .sys_hps_i2c1_scl_in_clk (i2c1_scl),
     .sys_gpio_bd_in_port (gpio_i[31:0]),
     .sys_gpio_bd_out_port (gpio_o[31:0]),
     .sys_gpio_in_export (gpio_i[63:32]),
